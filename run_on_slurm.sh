6426a91fac7a799201c87d606d2bdbd7c3f49129
axeljen <axejen@gmail.com>
2026-04-16 16:15:35 +0200

    some testing things

--- run_on_slurm.sh
+++ run_on_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH -A naiss2026-4-127
+#SBATCH -A naiss2025-22-471
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -t 4-00:00:00
@@ -9,9 +9,14 @@
 #SBATCH -o ./logs/%x-%j.out
 #SBATCH -e ./logs/%x-%j.error
 
-# write your code here
-
+# load nextflow module
 # ml nextflow
 
 # nextflow run main.nf --input testfiles/input.csv -profile dardel -resume
-nextflow run main.nf --input testfiles/input.csv -profile standard -resume
+nextflow run main.nf --input ../mapping_testfiles/input.csv -profile dardel -resume \
+    --popfile ../mapping_testfiles/popfile.txt \
+    --reference ../mapping_testfiles/reference/GCF_003339765.1_Mmul_10_NC_041770.1.fna \
+    --reads_dir ../mapping_testfiles/reads \
+    --name ../map_and_call_test/freebayes_test \
+    --scaffold_list ../mapping_testfiles/scaffolds.txt \
+    --slurm_account naiss2025-22-471
