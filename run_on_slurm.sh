8443882be907ec1a99ac8c0d11a5a3566929ec36
axeljen <axejen@gmail.com>
2026-04-16 16:34:27 +0200

    ml

--- run_on_slurm.sh
+++ run_on_slurm.sh
@@ -3,14 +3,14 @@
 #SBATCH -A naiss2025-22-471
 #SBATCH -p shared
 #SBATCH -n 1
-#SBATCH -t 4-00:00:00
+#SBATCH -t 0-05:00:00
 #SBATCH -J nf-varcall
 #SBATCH --mail-type=FAIL
 #SBATCH -o ./logs/%x-%j.out
 #SBATCH -e ./logs/%x-%j.error
 
 # load nextflow module
-# ml nextflow
+ml nextflow
 
 # nextflow run main.nf --input testfiles/input.csv -profile dardel -resume
 nextflow run main.nf --input ../mapping_testfiles/input.csv -profile dardel -resume \
