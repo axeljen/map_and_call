7b4ec31432698a0190a7f88bcd79350f76cc6c08
Axel Jensen <axejen@gmail.com>
2026-05-07 11:50:37 +0000

    testing

--- run_locally.sh
+++ run_locally.sh
@@ -17,4 +17,29 @@ nextflow run main.nf --input ../testfiles/input.csv -profile standard -resume \
     --reads_dir ../testfiles/reads/supersmall \
     --scaffold_list ../testfiles/scaffolds.txt \
     --variant_caller bcftools \
-    --outdir testout_bcftools
+    --outdir test/bcftools_cache
+
+# nextflow run preprocess_reads.nf --input ../testfiles/input.csv -profile standard -resume \
+#     --reference ../testfiles/reference/GCF_003339765.1_Mmul_10_4chroms.fa \
+#     --reads_dir ../testfiles/reads/supersmall \
+#     --scaffold_list ../testfiles/scaffolds.txt \
+#     --variant_caller bcftools \
+#     --skip_premapping_dedup \
+#     --outdir test/preprocess_test
+
+# nextflow run read_qc.nf -profile standard -resume \
+#     --reference ../testfiles/reference/GCF_003339765.1_Mmul_10_4chroms.fa \
+#     --reads_dir ../testfiles/reads/supersmall \
+#     --scaffold_list ../testfiles/scaffolds.txt \
+#     --variant_caller bcftools \
+#     --outdir test/readqc_test
+
+# nextflow run preprocess_and_map.nf --input ../testfiles/input.csv -profile standard -resume \
+#     --reference test/testout_bcftools/00_input_data/00_reference_genome/GCF_003339765.1_Mmul_10_4chroms.fa \
+#     --reads_dir ../testfiles/reads/supersmall \
+#     --scaffold_list ../testfiles/scaffolds.txt \
+#     --variant_caller bcftools \
+#     --skip_premapping_dedup \
+#     --outdir test/preprocess_and_map_test
+
+# test/testout_bcftools/00_input_data/00_reference_genome/GCF_003339765.1_Mmul_10_4chroms.fa 
