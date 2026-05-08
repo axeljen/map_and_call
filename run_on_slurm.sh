4f95bc01b72d40c1d3de150e239dcd12b7b8543d
axeljen <axejen@gmail.com>
2026-05-08 14:49:03 +0200

    update test script

--- run_on_slurm.sh
+++ run_on_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH -A naiss2025-22-471
+#SBATCH -A naiss2026-4-730
 #SBATCH -p shared
 #SBATCH -n 1
 #SBATCH -t 0-05:00:00
@@ -13,9 +13,9 @@
 ml nextflow
 
 # nextflow run main.nf --input testfiles/input.csv -profile dardel -resume
-nextflow run main.nf --input ../mapping_testfiles/input_2.csv -profile dardel_test -resume \
+nextflow run main.nf --input ../mapping_testfiles/input_2.csv -profile dardel -resume \
     --reference ../mapping_testfiles/reference/GCF_003339765.1_Mmul_10_4chroms.fa \
     --reads_dir ../mapping_testfiles/reads \
     --scaffold_list ../mapping_testfiles/scaffolds.txt \
     --name ../map_and_call_test/test_2 \
-    --slurm_account naiss2025-22-471
+    --slurm_account naiss2026-4-730
