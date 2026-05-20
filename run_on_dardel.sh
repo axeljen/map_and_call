6432e6d4ac31cfade830b9ce1f02af5afced8254
axeljen <axejen@gmail.com>
2026-05-20 11:32:43 +0200

    rename

---
+++ run_on_dardel.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -l
+
+#SBATCH -A naiss2026-4-730
+#SBATCH -p shared
+#SBATCH -c 4
+#SBATCH -t 0-05:00:00
+#SBATCH -J map-and-call
+#SBATCH --mail-type=FAIL
+#SBATCH -o ./logs/%x-%j.out
+#SBATCH -e ./logs/%x-%j.error
+
+# load nextflow and modules
+ml PDC/24.11 nextflow miniconda3
+
+# path to input file with sample and read information, see readme for details
+INPUT_CSV=../mapping_testfiles/input_2.csv
+# path to reference genome
+REFERENCE=../mapping_testfiles/reference/GCF_003339765.1_Mmul_10_4chroms.fa
+# path to reads directory. this argument can be omitted if full paths to reads are provided in the input csv (if so, remove the --reads_dir argument from the command below)
+READS_DIR=../mapping_testfiles/reads
+# Optionally provide a list of scaffols upon which to perform variant calling. If not provided, also remove the arguemnt from the command, and the pipeline will be run for the entire genome
+SCAFFOLD_LIST=../mapping_testfiles/scaffolds.txt
+# variant caller to use, either freebayes or bcftools
+VARIANT_CALLER=freebayes
+# chunk size in megabases for parallelizing variant calling. If many samples are included, this could be increased a bit to avoid too many jobs getting stuck in the queue perhaps.
+CHUNK_SIZE=20
+# output directory 
+OUTDIR=freebayes_testrun
+# account to use for slurm job submission
+SLURM_ACCOUNT=naiss2026-4-730
+
+
+# run the full pipeline
+nextflow run main.nf --input $INPUT_CSV \
+    -profile dardel -resume \
+    --reference $REFERENCE \
+    --reads_dir $READS_DIR \
+    --scaffold_list $SCAFFOLD_LIST \
+    --outdir $OUTDIR \
+    --slurm_account $SLURM_ACCOUNT \
+    --variant_caller $VARIANT_CALLER \
+    --chunk_size $CHUNK_SIZE
+
+##### Some additional, potentially useful flags:
+
+
+## --skip_premapping_dedup
+# By default, a a pcr deduplication step is run first on the raw reads, and then again on the mapped reads in the bam file.
+# in modern data with reasonably low levels of duplication rates (say, < ~ 30 %), this is most likely redundant and could potentially even
+# lead to over-deduplication and loss of data. So for modern, high quality data, this flag is recommended.
+
+## --conda_cachedir
+# by default, the conda environments created by the piepline will be stored in the .envs directory in the current working directory.
+# If intending to run the pipeline several times, you can save some time and space by setting this to a dedicated location that
+# you can then reuse for subseqeunt runs in different directories. Then, the environments doesn't need to be recreated.
+
+## --use_mamba
+# if you have a local installation of mamba, this argument will save some time when creating the conda environments. 
+# mamba is not available as a module on dardel, so the default is to use conda.
+
